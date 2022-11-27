7fe4f893a8c0f7bc39c5bc842eddc5f15bbc7ef3
kichinda <53686544+kichinda@users.noreply.github.com>
2022-11-27 21:19:49 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+let btn = document.getElementById("btn");
+        let speed = btn.value;
+        btn.innerHTML = val;
+        document.getElementById("result").innerHTML = "Value of the button: <b>" + speed+ "</b>";
+
+if (speed<70) {
+  prompt(Ok)
+}
+else  if (speed=>71++){
+  console.log(Points=2);
+}
+else {
+  console.log(Points=3);
+}
