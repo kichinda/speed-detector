60b1fccf2e6ad2fbcb8c2cc6bc1083101346edc2
kichinda <53686544+kichinda@users.noreply.github.com>
2022-12-14 13:12:28 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
+const form = document.getElementById("form")
+const input = document.getElementById("speedDetect")
+const message = document.getElementById("message")
 
-let btn = document.getElementById("btn");
-        let speed = btn.value;
-        btn.innerHTML = val;
-        document.getElementById("result").innerHTML = "Value of the button: <b>" + speed+ "</b>";
+form.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    checkSpeed(input) 
+});
 
-if (speed<70) {
-  prompt(Ok)
-}
-else  if (speed=>71++){
-  console.log(Points=2);
-}
-else {
-  console.log(Points=3);
+function checkSpeed(input){
+    console.log(input)
+    console.log(input.value)
+    const checkSpeed = input.value;
+
+    let points;
+    points = (checkSpeed - 70) / 5
+
+    if(checkSpeed <= 70) {
+        return message.textContent = 'OK'
+    }else if (checkSpeed > 70 && checkSpeed < 130){
+        return points
+    }else if(points >= 12){
+        return message.textContent = 'License suspended'
+    }
 }
