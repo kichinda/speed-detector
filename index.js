413982b9ce733553fdf971de93010a2ac8800c10
kichinda <53686544+kichinda@users.noreply.github.com>
2022-12-15 07:01:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Takes the input from the user.
 const form = document.getElementById("form")
 const input = document.getElementById("speedDetect")
 const message = document.getElementById("message")
@@ -11,6 +12,8 @@ function checkSpeed(input){
     console.log(input)
     console.log(input.value)
     const checkSpeed = input.value;
+    //variable points calculates points by subtracting current input speed value against the normal speed of 70km/h.
+    //eg current input to be 80km/h.  point calculated will be (80-70)/5 which equals to 2 points.
 
     let points;
     points = (checkSpeed - 70) / 5
@@ -19,6 +22,9 @@ function checkSpeed(input){
         return message.textContent = 'OK'
     }else if (checkSpeed > 70 && checkSpeed < 130){
         return points
+        //speed that generate a maximum of more than 12 points the licence will be suspended.
+        //12points equals 5*12 which is an addition to the normal speed by 60km/h
+        //60 +the normal 70 thats above 'safe' speed.
     }else if(points >= 12){
         return message.textContent = 'License suspended'
     }
